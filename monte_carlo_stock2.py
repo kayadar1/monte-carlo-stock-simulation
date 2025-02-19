095c310b282e265b50098ee48266facf9e9a45eb
David Kayadarma <d.kayadarma@mail.utoronto.ca>
2025-02-19 01:54:28 -0500

    Update monte_carlo_stock2.py

--- monte_carlo_stock2.py
+++ monte_carlo_stock2.py
@@ -25,9 +25,7 @@ def plot_simulation(prices, ticker, past_actual_prices, past_simulation):
     past_actual_prices = past_actual_prices[-252:]
     
     # Ensure continuity: set the last past simulated value to match today's price
-    # Adjust past simulation to smoothly connect to today's actual price
-    scaling_factor = past_actual_prices[-1] / avg_past_simulation[-1]
-    avg_past_simulation *= scaling_factor  # Scale past simulation to match the actual last price
+    avg_past_simulation[-1] = past_actual_prices[-1]
     
     # Days arrays
     days_past = np.arange(-252, 0)  # Last year's actual trading days
@@ -83,6 +81,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
